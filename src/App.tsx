389da34a68b13065fcab2219aaf058fd931b50d8
Rafael Servelo <rafael.servelo@hotmail.com>
2025-12-21 16:31:15 -0300

    commit update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
-    <div className="text-3xl" style={{ fontFamily: "var(--font-header)" }}>
-      Lua Minguante
+    <div className="text-center w-screen h-screen flex flex-col justify-center items-center gap-8 bg-primary">
+      <h1 className="text-9xl" style={{ fontFamily: "var(--font-header)" }}>
+        Lua Minguante
+      </h1>
+      <p className="text-6xl" style={{ fontFamily: "var(--font-title)" }}>
+        Lua Minguante
+      </p>
+      <p className="text-3xl" style={{ fontFamily: "var(--font-body)" }}>
+        Lua Minguante
+      </p>
     </div>
   );
 }
