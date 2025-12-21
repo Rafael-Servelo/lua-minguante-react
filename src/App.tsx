b18a32e654f02b2bbb4e6f6a6d02feaca7b730ad
Rafael Servelo <rafael.servelo@hotmail.com>
2025-12-21 20:26:08 -0300

    ajustes no visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Header from "./components/header";
+
 function App() {
   return (
-    <div className="text-3xl" style={{ fontFamily: "var(--font-header)" }}>
-      Lua Minguante
+    <div className="select-none flex flex-col items-center justify-center min-h-screen gap-4 w-screen h-screen bg-purple">
+      <Header></Header>
+      <div className="text-3xl text-rose text-title">Lua Minguante</div>
+      <div className="text-slate-200">Lua Minguante</div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <header className="text-6xl text-beige text-header">Lua Minguante</header>
+  );
+}
+
+export default Header;
